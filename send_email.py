f743cb55d5b4b3da15733c5a44905b8e63a5b6a5
Tanuj22 <agarwal.5@iitj.ac.in>
2019-11-22 02:55:57 +0530

    add  subject in email

--- send_email.py
+++ send_email.py
@@ -4,9 +4,13 @@ from sys import argv
 try:
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("sender_email", "pass") 
-    message = "Your password is " + argv[2]
-    s.sendmail("sender_email", argv[1], message) 
+    SENDER = "sender_email" # Enter sender email id
+    PASSWORD = "pass"  # Enter password
+    s.login(SENDER, PASSWORD) 
+    SUBJECT = "Forgot password"
+    TEXT = "Your password is " + argv[2]
+    message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+    s.sendmail(SENDER, argv[1], message) 
     s.quit() 
 except:
     print ('Something went wrong...')
