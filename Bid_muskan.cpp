ca77cebf04f47168518c2e5ce866c28dcddd7d58
Muskan <agrawal.9@iitj.ac.in>
2019-11-22 23:42:53 +0530

    Finally Bid_muskan updated

--- Bid_muskan.cpp
+++ Bid_muskan.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<fstream>
-#include<string>
-#include<fstream>
+#include<cstring>
 #include "color.h"
 
 using namespace std;
@@ -12,94 +11,137 @@ Color::Modifier green(Color::FG_GREEN);
 Color::Modifier def(Color::FG_DEFAULT);
 
 int count=0;
+
 class bid
 {
 	private:
 	string bid_title, bid_description, bid_amount;
 	string deadline_date;
-	char agree;
-	string bid_no;
+	int bid_no;
 	public:
 	void get_description(string s)
 	{
-		strcpyIbid_description,s);
+		int i=0;
+
+	for(i=0;i<s.size();i++)
+		bid_description[i]=s[i];
+
+	}
+	void get_bid_no(int s)
+	{
+		bid_no=s;
 	}
 	void get_title(string s)
-	{ 
-		strcpy(bid_title,s);
-	}
+	{
+		int i=0;
+
+	for(i=0;i<s.size();i++)
+		bid_title[i]=s[i];
+		}
 	int get_amount(string s)
 	{
-		if(s=='0')
-			{cout<<red<<endl<<"Enter valid amount"<<endl;
+
+		for(int i=0;i<s.size();i++)
+    {
+      if((s[i]-0 >= 65 && s[i]-0<=90) ||(s[i] -0 >= 97 && s[i]-0<=122))
+        {
+          cout<<red<<"\nEnter valid amount\n"<<def;
+          return 0;
+        }
+    }
+	if(s[0]=='0')
+			{cout<<red<<endl<<"Enter valid amount\n"<<def;
 			return 0;}
-		else
-			strcpy(bid_amount,s);
+	else
+	{		int i=0;
+
+	for(i=0;i<s.size();i++)
+		bid_amount[i]=s[i];
+
 		return 1;
-	}
+	}}
 
 	int deadline(string s)
-	{
-		if(s[0]>=4 || s[0]==3 && s[1]>=2 || s[2]>=2 || s[2]==1 && s[3]>=3)
+	{	for(int i=0;i<s.size();i++)
+    {
+      if(!(s[i] -0 >= 48 && s[i]-0<= 57))
+        {
+          cout<<red<<"\nEnter in valid format\n"<<def;
+          return 0;
+        }
+    }
+		if(s[0]-0>=51 || s[2]-0>=49 || (s[2]-0==49 && s[3]-0>=50) || (s[0]-0==51 && s[1]-0>=49) )
 		{
 			cout<<red<<endl<<"Enter in valid format"<<endl;
 			return 0;
 		}
 		else
-			strcpy(deadline_date,s);
-		return 1;
+	{
+			int i=0;
+
+	for(i=0;i<s.size();i++)
+			deadline_date[i]=s[i];
+	return 1;
+
+		}
+
 	}
 
-	
-	
+
+
 };
-		
+
 int main()
-{	string bid_title, bid_description, bid_amount;
+{
+	string bid_title, bid_description, bid_amount;
 	string deadline_date;
+	int bid_no;
 	char agree;
-	string bid_no;
 	char create_bid;
-	cout<<blue<<"\n\nCreate Bid\n (Y/N)\n\n";
+	cout<<blue<<"\n\nCreate Bid\n (Y/N)\n\n"<<def;
 	cin>>create_bid;
-	Bid b;
+	bid b;
 	int x;
 	if(create_bid=='y' || create_bid=='Y')
 	{
-			cout<<blue<<"\nNow enter bid details\n";
-			cout<<blue<<"\nEnter bid description\n";
-			getline(cin,bid_description);
-			b.get_description(&bid_description);
-			cout<<blue<<"\nEnter bid title\n";
-			getline(cin,bid_title);
-			b.get_title(&bid_title);
+			cout<<blue<<"\nNow enter bid details\n"<<def;
+			cout<<blue<<"\nEnter bid description"<<def;
+			cin>>bid_description;
+			//scanf("%[^\n]s",bid_description);
+			b.get_description(bid_description);
+			cout<<blue<<"\nEnter bid title\n"<<def;
+			cin>>bid_title;			//scanf("%[^\n]s",bid_title);
+			b.get_title(bid_title);
 			do
 			{
-				cout<<blue<<"\nEnter bid amount";
+				cout<<blue<<"\nEnter bid amount"<<def;
 				cin>>bid_amount;
+				//scanf("%[^\n]s",bid_amount);
 			}
-			while(b.get_amount(&bid_amount)==0);
-			
+			while(b.get_amount(bid_amount)==0);
+
 			do
 			{
-				cout<<blue<<"\nEnter deadline date in dd/mm/yy format\n";
-				getline(cin,deadline_date);
+				cout<<blue<<"\nEnter deadline date in dd/mm/yy format\n"<<def;
+				//scanf("%[^\n]s",deadline_date);
+				cin>>deadline_date;
 			}
-			while(b.deadline(&deadline_date)==0);
+			while(b.deadline(deadline_date)==0);
 
-			cout<<blue<<"\n Confirm?\n (Y/N) \n";
+			cout<<blue<<"\n Confirm?\n (Y/N) \n"<<def;
 			cin>>agree;
 			if(agree=='Y' || agree=='y')
-			{	count++;
-				cout<<"\nYour bid is created and waiting for the final confirmation from the admin.\n";
-				bid_no=count;
+			{
+        // count++;
+				cout<<green<<"\nYour bid is created and waiting for the final confirmation from the admin.\n"<<def;
+				bid_no=0;
+				b.get_bid_no(bid_no);
 				ofstream fp;
 
 			        fp.open("Bid_List.txt", ios::app);
 			        fp.write((char*)&b, sizeof(b));
-			}	
-	
+			}
+	}
+
 	return 0;
 }
-
-		 
